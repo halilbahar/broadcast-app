bbc690f770c13f9a8d4885d03efea243ca981ce4
halilbahar <halil.s.bahar@gmail.com>
2020-03-28 20:16:16 +0100

    Delete streams

--- app/app.js
+++ app/app.js
@@ -11,7 +11,7 @@ const key = process.env.KEY || 'default';
 
 const fs = require('fs');
 
-if(!fs.existsSync('./audio')) {
+if (!fs.existsSync('./audio')) {
     fs.mkdirSync('./audio');
 }
 
@@ -20,7 +20,7 @@ app.use(upload.single('broadcast'));
 app.use((req, res, next) => {
     if (!req.body || !req.body.key) {
         res.status(401).json(new Message('No key was provided.'));
-    } else if(req.body.key !== key) {
+    } else if (req.body.key !== key) {
         res.status(401).json(new Message('A wrong key was provided.'));
     } else {
         next();
@@ -29,12 +29,26 @@ app.use((req, res, next) => {
 
 let currentCommand = null;
 app.post('/new-broadcast', (req, res) => {
-    if(currentCommand != null) {
+    if (currentCommand != null) {
         currentCommand.stopStream();
+        currentCommand = null;
     }
     currentCommand = new Ffmpeg(req.file.buffer);
     currentCommand.startStream();
     res.status(200).json(new Message('Started new stream!'));
+});
+
+app.post('/delete-broadcast', (req, res) => {
+    let message = new Message();
+    if (currentCommand != null) {
+        currentCommand.stopStream();
+        currentCommand = null;
+        message.setMessage('Stopped current stream!');
+    } else {
+        message.setMessage('Could not stop streaming. No stream was running.')
+    }
+
+    res.status(200).json(message);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
--- app/message.js
+++ app/message.js
@@ -2,4 +2,12 @@ module.exports = class Message {
     constructor(message) {
         this.message = message;
     }
+
+    getMessage() {
+        return this.message;
+    }
+
+    setMessage(message) {
+        this.message = message;
+    }
 }

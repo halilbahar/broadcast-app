d8a917291099d8bcb4d86bc716ec9d71b4cf78df
halilbahar <halil.s.bahar@gmail.com>
2020-04-02 12:34:43 +0200

    Wait always 200ms

--- src/middleware/authentication-middleware.js
+++ src/middleware/authentication-middleware.js
@@ -1,9 +1,11 @@
 const { key } = require('../global');
 
 module.exports = (req, res, next) => {
-    if (!req.headers.key || req.headers.key !== key) {
-        res.sendStatus(401);
-    } else {
-        setTimeout(() => next(), 250);
-    }
+    setTimeout(() => {
+        if (!req.headers.key || req.headers.key !== key) {
+            res.sendStatus(401);
+        } else {
+            next()
+        }
+    }, 200)
 }
